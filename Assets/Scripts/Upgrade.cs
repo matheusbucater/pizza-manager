5bba333cd221339f128ad6a16bf6285d74a9cc86
matheusbucater <matheusp.bucater@gmail.com>
2024-10-20 09:56:10 -0300

    Ajusta o balanceamento custo/beneficio dos upgrades

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -7,36 +7,50 @@
 // Bibliotecas
 //===================================================================================================
 using System;
-using System.Collections.Generic;
 
 public static class Upgrade
 {
     //===============================================================================================
-    // Declaração de Variáveis
+    // Declaração de Constantes
     //===============================================================================================
+    private const double _ovenUpgradeBase = 0.75;
+    private const double _ovenUpgradeRate = 1.5;
+    private const double _ovenCostBase = 20;
+    private const double _ovenCostRate = 1.02;
+
+    private const double _customerUpgradeBase = 0;
+    private const double _customerUpgradeRate = 1.1;
+    private const double _customerCostBase = 30;
+    private const double _customerCostRate = 1.01;
+
+    private const double _pizzaUpgradeBase = 9;
+    private const double _pizzaUpgradeRate = 15;
+    private const double _pizzaCostBase = 40;
+    private const double _pizzaCostRate = 1.11;
 
 
     //===============================================================================================
     // Métodos
     //===============================================================================================
     private static double OvenUpgrade(int levelIndex) {
-        return Math.Round(3.75 * (levelIndex + 1), 2);
+        return Math.Round(_ovenUpgradeBase + (_ovenUpgradeRate * levelIndex), 2);
     }
     private static double OvenCost(int levelIndex) {
-        return Math.Round(0.75 * Math.Pow(1.75, levelIndex), 2);
+        return Math.Round(_ovenCostBase * Math.Pow(_ovenCostRate, levelIndex), 2);
     }
     private static double CustomerUpgrade(int levelIndex) {
-        return Math.Round(2.5 * (levelIndex + 1), 2);
+        return Math.Round(_customerUpgradeBase + ( _customerUpgradeRate * levelIndex), 2);
     }
     private static double CustomerCost(int levelIndex) {
-        return Math.Round(0.5 * Math.Pow(1.5, levelIndex), 2);
+        return Math.Round(_customerCostBase * Math.Pow(_customerCostRate, levelIndex), 2);
     }
     private static double PizzaUpgrade(int levelIndex) {
-        return Math.Round(9 * Math.Pow(1.07,levelIndex), 2);
+        return Math.Round(_pizzaUpgradeBase + (_pizzaUpgradeRate * levelIndex), 2);
     }
     private static double PizzaCost(int levelIndex) {
-        return Math.Round(18 * Math.Pow(1.07,levelIndex), 2);
+        return Math.Round(_pizzaCostBase * Math.Pow(_pizzaCostRate, levelIndex), 2);
     }
+
     public static double GetUpgrade(int upgradeIndex, int levelIndex) {
         return upgradeIndex switch
         {
